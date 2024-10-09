e4d3a4dedad4581d5c11057a0f84c128fab30741
Sascha Dobschal <saschadobschal@macbookright--sascha.fritz.box>
2024-10-09 14:54:51 +0200

    add server and docker compose for database

---
+++ server/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.allopen") version "1.9.10"
+    id("io.quarkus")
+    id("org.jetbrains.kotlin.plugin.noarg") version "1.9.10"
+
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-arc")
+
+    // Validation
+    implementation("io.quarkus:quarkus-hibernate-validator")
+
+    // Kotlin
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Cache
+    implementation("io.quarkus:quarkus-cache")
+
+    // DB
+    implementation("io.quarkus:quarkus-hibernate-orm-panache-kotlin")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-flyway")
+
+    // scheduler
+    implementation("io.quarkus:quarkus-scheduler")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    // REST
+    implementation("io.quarkus:quarkus-rest-client-reactive-jackson")
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // TEST
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.quarkiverse.mockk:quarkus-junit5-mockk:2.1.0")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "eu.dobschal"
+version = "0.1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+allOpen {
+    annotation("jakarta.ws.rs.Path")
+    annotation("jakarta.enterprise.context.ApplicationScoped")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+    annotations("jakarta.persistence.Entity")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.javaParameters = true
+    // For creation of default methods in interfaces(rest service)
+    kotlinOptions.freeCompilerArgs = listOf("-Xjvm-default=all")
+}
---
+++ server/src/main/kotlin/eu/dobschal/resource/VersionResource.kt
@@ -0,0 +1,23 @@
+package eu.dobschal.resource
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.eclipse.microprofile.config.inject.ConfigProperty
+
+@Path("/api/v1/version")
+@Produces(MediaType.APPLICATION_JSON)
+class VersionResource {
+
+    @ConfigProperty(name = "quarkus.application.version")
+    lateinit var version: String
+
+    @GET
+    fun apiVersion(): Response {
+        return Response
+            .ok(version)
+            .build()
+    }
+}
