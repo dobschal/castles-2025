fc1ff3a6011fd83a2d054324a3fd2d7082f138db
Sascha Dobschal <saschadobschal@macbookright--sascha.fritz.box>
2024-10-11 13:35:01 +0200

    add no cache flag

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
 git fetch
 git reset --hard origin/main
-docker compose build server
+docker compose build server --no-cache
 docker compose down server
 docker compose up server -d
-docker compose build client
+docker compose build client --no-cache
 docker compose down client
 docker compose up client -d
