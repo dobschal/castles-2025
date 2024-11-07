ce451434dddbb89dc27fbed9874e61d2007ea078
Sascha Dobschal <saschadobschal@macbookright--sascha.fritz.box>
2024-11-07 19:53:37 +0100

    ssh to restart server

--- restart.sh
+++
@@ -1,9 +0,0 @@
-git fetch
-git reset --hard origin/main
-docker compose build server --no-cache
-docker compose down server
-docker compose up server -d
-docker compose build client --no-cache
-docker compose down client
-docker compose up client -d
-docker system prune -f
